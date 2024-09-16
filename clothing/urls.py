143951b64516366b584ef71039e6c85269c41cac
Hasan-XS <ha3an.majidii@gmail.com>
2024-09-16 21:40:39 +0330

    create tshirt product url in urls.py on clothing app

--- clothing/urls.py
+++ clothing/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import clothings_view, shoes_view, accessory_view, pants_view, hat_view
+from .views import clothings_view, shoes_view, accessory_view, pants_view, hat_view,tshirt_view
 
 urlpatterns = [
     path("", clothings_view, name="clothings"),
+    path("tshirt/", tshirt_view, name="tshirt"),
     path("shoes/", shoes_view, name="shoes"),
     path("accessory/", accessory_view, name="accessory"),
     path("pants/", pants_view, name="pants"),
