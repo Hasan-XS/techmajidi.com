93c37b1c9f63cc0d471a382764aafadabd88b450
Hasan-XS <ha3an.majidii@gmail.com>
2024-09-18 02:45:42 +0330

    import all clothing mode in post app in views.py and show tshirt in posts(index.html) page

--- post/views.py
+++ post/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 from .models import Blog, Post
+from clothing.models import Tshirt, Shoes, Pants, Accessory, Hat
 from django.views.generic import DetailView
 
 # from django.shortcuts import render_to_response
@@ -16,9 +17,11 @@ def posts_view_index(request):
     blogs = Blog.objects.all()
     posts_list = list(reversed(posts))[:4]
     blogs_list = list(reversed(blogs))[:4]
+    tshirts = list(reversed(Tshirt.objects.all()))[:4]
     context = {
         "posts": posts_list,
         "blogs": blogs_list,
+        "tshirts":tshirts,
     }
     return render(request, "posts/index.html", context)
 
@@ -31,6 +34,7 @@ def posts_view(request):
         "posts": posts_list,
     }
     return render(request, "posts/posts.html", context)
+
 
 def post_view_details(request, pk):
     post = get_object_or_404(Post, id=pk)
